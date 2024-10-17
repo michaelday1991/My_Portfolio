074adb50f6407f06577ebbc7cfa94614817cb6ed
Michael Day <michaelday1991@hotmail.co.uk>
2024-10-17 10:55:41 +0100

    Added a screenshot to Images and content to the JSON file

--- js.js
+++ js.js
@@ -1,6 +1,15 @@
 fetch('data.json')
-.then(response => response.json())
-.then(date => {
-    const container = document.querySelector("project-container");
-    console.log(container)
+    .then(response => response.json())
+    .then(data => {
+        const container = document.querySelector("#project-container");
+        
+        data.projects.forEach(project => {
+            container.innerHTML += `
+            <div class= 'project'>
+                <h3>${project.name}</h3>
+                <img src='${project.img}' alt="Picture of a website" />
+                <p class= 'Description'>${project.desc}</p>
+                </div>`;
+        });
 })
+.catch(error => console.error('Error fetching data:', error));
